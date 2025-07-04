77b1bb7fb8da2e93e6b2c913aeb66e8340e66f35
agx <agxr@tuta.io>
2025-07-04 21:29:44 +0500

    Красиво выделил структуры

--- UaruKernel.lean
+++ UaruKernel.lean
@@ -7,19 +7,20 @@ open Lean Json ToJson FromJson
 
 namespace UaruKernel
 
---- тгшная хуйня
-
--- юзер
+----------------------------------------
 structure User where
   userId     : Option Nat := none
   firstName  : Option String := none
   lastName   : Option String := none
   username   : Option String := none
-  isAsshole  : Option Bool := none
+  isPremium  : Option Bool := none
   isGuest    : Option Bool := none
   deriving FromJson
 
--- мсг
+structure NewMember where
+  user : User
+  deriving FromJson
+
 structure Message where
   messageId      : Option Nat := none
   fromUser       : Option User := none
@@ -30,22 +31,56 @@ structure Message where
   isSticker      : Option Bool := none
   deriving FromJson
 
---- инпут собсна
+--- Возможные event:
+-- message sent
 structure IPCInput where
-  event   : Option String := none
+  event   : Option String := "empty"
   message : Option Message := none
+  date    : Option String := none
   deriving FromJson
+----------------------------------------
 
---- аутпут собсна
-structure IPCResponse where
-  status : String
-  output : String
+
+----------------------------------------
+structure SendMessageAction where
+  text       : String := "empty"
+  replyToId  : Option Nat := none 
   deriving ToJson 
+
+structure BanUserAction where
+  userId : Nat := 0 
+  deriving ToJson 
+
+structure MuteUserAction where
+  userId : Nat := 0 
+  duration : Nat := 300 -- in seconds
+  deriving ToJson 
+
+structure PinMessageAction where
+  messageId : Nat := 0 
+  deriving ToJson 
+
+structure DeleteMessageAction where
+  messageId : Nat := 0 
+  deriving ToJson 
+
+--- Возможные status:
+-- unknown error
+-- ok
+structure IPCResponse where
+  status         : String := "unknown error"
+  sendMessage    : Option SendMessageAction := none 
+  pinMessage     : Option PinMessageAction := none
+  deleteMessage  : Option DeleteMessageAction := none
+  muteUser       : Option MuteUserAction := none 
+  banUser        : Option BanUserAction := none 
+  deriving ToJson 
+----------------------------------------
 
 def handleCommand (jsonStr : String) : String :=
   match Json.parse jsonStr >>= fromJson? with
   | .ok (input : IPCInput) =>
-    if input.event == some "message_sent" && input.message.bind (·.text) == some "/start" then
+    if input.event == some "message sent" && input.message.bind (·.text) == some "/start" then
       "bot working"
     else
       "unknown command"
